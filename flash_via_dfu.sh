5c6261ead936c58c86f741e6b707b8dbdd01a67a
Ivan Gagis <igagis@gmail.com>
2024-09-08 18:23:39 +0300

    stuff

--- flash_via_dfu.sh
+++ flash_via_dfu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+echo "get bootloader-version"
+dfu-programmer atmega32u4 get --debug 10 bootloader-version
+
 echo "erase"
 dfu-programmer atmega32u4 erase
+
 echo "flash"
 dfu-programmer atmega32u4 flash qmk_firmware/kb_default.hex
